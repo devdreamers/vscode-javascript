99cd73cd51699de2eff37b6e15d90151793eec9e
SEM <jul0218@gmail.com>
2021-02-18 21:53:44 +0900

    day1 modified variable.js

--- variable.js
+++ variable.js
@@ -1,3 +1,31 @@
+// 1. Use strict
+// added in ES 5
+// use this for Valina Javascript.
+'use strict';
+
+// 2. Variable
+// let (added in ES6)
+let globalName = 'global name';
+{
+    let name = 'ellie';
+    console.log(name);
+    name = 'hello';
+    console.log(name);
+    console.log(globalName);
+}
+console.log(name);
+console.log(globalName);
+
+// var (don't ever use this!)
+// var hoisting (move declaration from bottom to top)
+// has no block scope
+{
+    console.log(age);
+    age = 4;
+    var age;
+}
+console.log(age);
+
 // 3. Constant, r(read only)
 // use const whenever possible.
 // only use let if variable needs to change.
