64091b0389c43e8619556160952905416a0c3e82
devdreamers <jul0218@gmail.com>
2021-02-19 18:00:39 +0900

    day2

--- operator.js
+++ operator.js
@@ -19,3 +19,33 @@ const preIncrement = ++counter;
 // counter = conter + 1;
 // preIncrement = counter;
 console.log(`preIncrement: ${preIncrement}, counter: ${counter}`);
+
+// 4. Assignment operators
+let x = 3;
+let y = 6;
+x += y; // x = x + y;
+x -= y;
+x *= y;
+x /= y;
+
+// 5. Comparison operators
+console.log(10 < 6); // less than
+console.log(10 <= 6); // less than or equal
+console.log(10 > 6); // greater than
+console.log(10 >= 6); // greater than or equal
+
+
+// 6. Logical operators: || (or), && (and), ! (not)
+const value1 = false;
+const value2 = 4 < 2;
+
+// || (or)
+console.log(`or: ${value1 || value2 || check()}`);
+
+function check() {
+    for (let i = 0; i < 10; i++) {
+        //wasting time
+        console.log('omg');
+    }
+    return true;
+}
